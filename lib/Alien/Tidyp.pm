7727e5611d17d053603d921c83b68dbe711cbc21
kmx <kmx@cpan.org>
2010-04-06 08:55:08 +0200

    Last changes before 0.99.6 release

--- lib/Alien/Tidyp.pm
+++ lib/Alien/Tidyp.pm
@@ -13,11 +13,11 @@ Alien::Tidyp - building and using tidyp library - L<http://www.tidyp.com>
 
 =cut
 
-our $VERSION = '0.99.5';
+our $VERSION = '0.99.6';
 
 =head1 VERSION
 
-Version 0.99.5 of Alien::Tidyp uses I<tidyp> sources v0.99 + some patches.
+Version 0.99.6 of Alien::Tidyp uses I<tidyp> sources v0.99 + some patches.
 
 Specifically this commit: L<http://github.com/petdance/tidyp/commit/749825bc9>
 
