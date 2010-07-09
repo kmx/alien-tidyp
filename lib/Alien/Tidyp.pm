9d3c94e1d48e88cc070b5fa051737dc065b2febc
kmx <kmx@cpan.org>
2010-07-09 17:16:21 +0200

    prepare for next version

--- lib/Alien/Tidyp.pm
+++ lib/Alien/Tidyp.pm
@@ -13,11 +13,11 @@ Alien::Tidyp - Building, finding and using tidyp library - L<http://www.tidyp.co
 
 =cut
 
-our $VERSION = 'v1.2.6';
+our $VERSION = 'v1.2.7';
 
 =head1 VERSION
 
-Version v1.2.6 of Alien::Tidyp uses I<tidyp> sources v1.02.
+Version v1.2.7 of Alien::Tidyp uses I<tidyp> sources v1.02.
 
 =head1 SYNOPSIS
 
